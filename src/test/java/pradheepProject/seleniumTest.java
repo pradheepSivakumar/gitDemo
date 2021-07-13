861cb1435eace53c5d67015e6d623814e783c9ce
pradheepSivakumar <pradheep.s1109@gmail.com>
2021-07-13 10:06:54 +0530

    commit from asian time zone

--- src/test/java/pradheepProject/seleniumTest.java
+++ src/test/java/pradheepProject/seleniumTest.java
@@ -6,6 +6,11 @@ public class seleniumTest {
 @Test
 public void browserAutomation() {
 	System.out.println("browserAutomation");
+	
+	System.out.println("Web based automation");
+	
+	
+	System.out.println("Replaced mercury in the market");
 }
 @Test
 public void openElementsUI() {
