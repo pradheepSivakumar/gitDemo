851f9cd8f77577fcab8c95b025e9d3c828761453
pradheepSivakumar <pradheep.s1109@gmail.com>
2021-07-13 09:38:17 +0530

    first commit

---
+++ src/test/java/pradheepProject/AppiumTest.java
@@ -0,0 +1,13 @@
+package pradheepProject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	public void postJira() {
+		//System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
---
+++ src/test/java/pradheepProject/seleniumTest.java
@@ -0,0 +1,14 @@
+package pradheepProject;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+@Test
+public void browserAutomation() {
+	System.out.println("browserAutomation");
+}
+@Test
+public void openElementsUI() {
+	System.out.println("openElementsUI");
+}
+}
